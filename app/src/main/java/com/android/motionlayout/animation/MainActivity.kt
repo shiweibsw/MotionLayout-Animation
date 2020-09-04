99a76bd6ded3361d4c65059bb3d6c66e28e38599
shiweibsw <shiweibsw@gmail.com>
2020-09-04 14:26:54 +0800

    init

--- app/src/main/java/com/android/motionlayout/animation/MainActivity.kt
+++ app/src/main/java/com/android/motionlayout/animation/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.android.motionlayout.animation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private var doShowPaths = false
+
+    private val dataset: Array<DemosAdapter.Demo> = arrayOf(
+        DemosAdapter.Demo(
+            "Basic Example (1/3)",
+            "Basic motion example using referenced ConstraintLayout files",
+            R.layout.motion_01_basic
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = DemosAdapter(dataset)
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerview).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        showPaths.setOnCheckedChangeListener(this)
+    }
+
+    override fun onCheckedChanged(p0: CompoundButton?, value: Boolean) {
+        doShowPaths = value
+    }
+
+    fun start(activity: Class<*>, layoutFileId: Int) {
+        val intent = Intent(this, activity).apply {
+            putExtra("layout_file_id", layoutFileId)
+            putExtra("showPaths", doShowPaths)
+        }
+        startActivity(intent)
     }
 }
